217554e8dbce52ffeb40408271804a85a0f89f59
nospighost <nospighost@gmail.com>
2025-06-18 17:27:08 +0200

    /ce add|remove Neue Fähigkeiten: - Unendliche Kohle - WoodCutter mit Rechtsklick Bonemeal

---
+++ src/main/java/de/Main/editEffect/gui/UpgradeGUIAxt.java
@@ -0,0 +1,38 @@
+package de.Main.editEffect.gui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class UpgradeGUIAxt {
+
+    public static Inventory open(Player player) {
+        Inventory upgradeguiaxt = Bukkit.createInventory(null, 27, "§cAxt Upgrades");
+
+        ItemStack effezienz = new ItemStack(Material.ENCHANTED_BOOK);
+        ItemMeta effezienzMeta = effezienz.getItemMeta();
+        effezienzMeta.setDisplayName("§cEffizienz Upgrade");
+        effezienz.setItemMeta(effezienzMeta);
+        upgradeguiaxt.setItem(0, effezienz);
+
+        ItemStack haltbarkeit = new ItemStack(Material.NETHERITE_INGOT);
+        ItemMeta haltbarkeitMeta = haltbarkeit.getItemMeta();
+        haltbarkeitMeta.setDisplayName("§cHaltbarkeit Upgrade");
+        haltbarkeit.setItemMeta(haltbarkeitMeta);
+        upgradeguiaxt.setItem(1, haltbarkeit);
+
+        ItemStack Sharpness = new ItemStack(Material.IRON_SWORD);
+        ItemMeta SharpnessMeta = Sharpness.getItemMeta();
+        SharpnessMeta.setDisplayName("§cSharpness Upgrade");
+        Sharpness.setItemMeta(SharpnessMeta);
+        upgradeguiaxt.setItem(2, Sharpness);
+
+
+
+
+        return upgradeguiaxt;
+    }
+}
---
+++ src/main/java/de/Main/editEffect/gui/UpgradeGUIPickaxe.java
@@ -0,0 +1,34 @@
+package de.Main.editEffect.gui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class UpgradeGUIPickaxe {
+
+    public static Inventory open(Player player) {
+        Inventory upgradeguipickaxe = Bukkit.createInventory(null, 27, "§cSpitzhacken Upgrades");
+
+        ItemStack effezienz = new ItemStack(Material.ENCHANTED_BOOK);
+        ItemMeta effezienzMeta = effezienz.getItemMeta();
+        effezienzMeta.setDisplayName("§cEffizienz Upgrade");
+        effezienz.setItemMeta(effezienzMeta);
+        upgradeguipickaxe.setItem(0, effezienz);
+
+        ItemStack haltbarkeit = new ItemStack(Material.NETHERITE_INGOT);
+        ItemMeta haltbarkeitMeta = haltbarkeit.getItemMeta();
+        haltbarkeitMeta.setDisplayName("§cHaltbarkeit Upgrade");
+        haltbarkeit.setItemMeta(haltbarkeitMeta);
+        upgradeguipickaxe.setItem(1, haltbarkeit);
+
+        ItemStack Glück = new ItemStack(Material.TOTEM_OF_UNDYING);
+        ItemMeta GlückMeta = Glück.getItemMeta();
+        GlückMeta.setDisplayName("§cGlück Upgrade");
+        Glück.setItemMeta(GlückMeta);
+        upgradeguipickaxe.setItem(2, Glück);
+        return upgradeguipickaxe;
+    }
+}
